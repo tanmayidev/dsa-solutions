9f9dc4b710b6c788fe189d3611d0749c4ad16afa
tanmayidev <tanmayi.d.99@gmail.com>
2024-01-08 19:30:19 +0530

    feat: minimum different subset partition

---
+++ topic-based/dynamic-programming/01-0-1-knapsack/05-min-diff-subset-partition.cpp
@@ -0,0 +1,55 @@
+// Question Link : https://www.geeksforgeeks.org/problems/minimum-sum-partition3317/1?
+
+/*
+ Time Complexity : 
+ Space Complexity : 
+*/
+
+class Solution{
+
+  public:
+	int minDifference(int arr[], int N)  { 
+	    
+	    int sum = 0;
+	    for(int i = 0; i < N ; i++ ) {
+	        sum += arr[i];
+	    }
+	    
+	    bool dp[N+1][sum+1];
+	    
+	    // subset sum
+	    for(int s=0; s < sum+1; s++) {
+	        dp[0][s] = false;
+      }
+	    
+      dp[0][0] = true; // n==0 and s==0 i.e., nullset is possible
+	    
+	    for(int n=1; n < N+1; n++) {
+	        for(int s=0; s < sum+1; s++) { // can make it (sum+1)/2
+	            if(arr[n-1] <= s) {
+	                dp[n][s] = dp[n-1][s - arr[n-1]] || dp[n-1][s];
+	            }
+	            else {
+	                dp[n][s] = dp[n-1][s];
+	            }
+	        }
+	    }
+	    
+	    vector<int> rangeArr;
+	    // taking sum+1 because it is the complete width of the table (dp[N+1][sum+1])
+      // it can also be i<= sum/2
+	    for(int i=0; i< ((sum+1)/2); i++) { 
+	        if(dp[N][i]) {
+	            rangeArr.push_back(i);
+	        }
+	    }
+	    
+	    // iterate from end to and break whenever true is found
+	    int minDiff = INT_MAX;
+	    for(int i = 0; i < rangeArr.size(); i++ ) {
+	        minDiff = min(minDiff, sum - 2*rangeArr[i]);
+	    }
+	    
+	    return minDiff;
+	} 
+};
