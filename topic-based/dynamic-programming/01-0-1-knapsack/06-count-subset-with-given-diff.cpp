8ae813e6287c6675e3984ed7afdd44309ed0ec58
tanmayidev <tanmayi.d.99@gmail.com>
2024-01-08 19:30:40 +0530

    feat: count subset with given difference

---
+++ topic-based/dynamic-programming/01-0-1-knapsack/06-count-subset-with-given-diff.cpp
@@ -0,0 +1,61 @@
+// Question Link : https://practice.geeksforgeeks.org/problems/partitions-with-given-difference/1
+
+/*
+ Time Complexity : 
+ Space Complexity : 
+*/
+
+class Solution {
+  public:
+    int countPartitions(int N, int D, vector<int>& arr) {
+        // S1 - S2 = D
+        // S1 + S2 = Sum
+        //----------------
+        // S1 + S1 = D + Sum
+        //----------------
+
+        // 2*S1 = D + Sum
+        // S1 = (D + Sum ) / 2
+        // The number of subsets that can create S2 will be same as number of subsets that can create S1
+        // So the count of subsets of S2 is the solution
+        
+        int mod = 1000000007;
+        
+        int arrSum = 0;
+        for(int s = 0; s < N; s++ ) {
+            arrSum = (arrSum + arr[s]);
+        }
+        
+        // very imp condition
+        if((arrSum+D)%2 != 0) {
+            return 0;
+        }
+        
+        int targetSum = (arrSum + D)/ 2;
+        
+        vector<vector<int>> dp(N+1, vector<int>(targetSum+1));
+        
+        // base conditions
+        for(int s=0; s < targetSum+1; s++ ) {
+            dp[0][s] = 0; // n == 0 and s == any value
+        }
+        
+        dp[0][0] = 1; // s == 0 and n == 0 i.e., nullset is possible
+        
+        // main condition
+        
+        for(int n=1; n < N+1; n++) {
+            for(int s =0; s < targetSum+1; s++) {
+                if(arr[n-1] <= s) {
+                    dp[n][s] = (dp[n-1][s - arr[n-1]] + dp[n-1][s])%mod;
+                }
+                else {
+                    dp[n][s] = dp[n-1][s];
+                }
+            }
+        }
+        
+        return dp[N][targetSum];
+        
+    }
+};
