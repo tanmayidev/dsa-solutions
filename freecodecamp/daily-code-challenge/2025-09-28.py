54ec7725e03cab127e5e905f6ed5310f4c78821c
Tanmayi D <60173146+tanmayidev@users.noreply.github.com>
2025-09-30 15:04:01 +0530

    feat: add 2025-09-28.py

---
+++ freecodecamp/daily-code-challenge/2025-09-28.py
@@ -0,0 +1,24 @@
+"""
+CSV Header Parser
+
+Given the first line of a comma-separated values (CSV) file, return an array containing the headings.
+- The first line of a CSV file contains headings separated by commas.
+- Remove any leading or trailing whitespace from each heading.
+
+Tests :
+1. get_headings("name,age,city") should return ["name", "age", "city"].
+2. get_headings("first name,last name,phone") should return ["first name", "last name", "phone"].
+3. get_headings("username , email , signup date ") should return ["username", "email", "signup date"].
+"""
+
+def get_headings(csv):
+    return [heading.strip() for heading in csv.split(',')]
+
+# Brute Force
+
+def get_headings(csv):
+    headings = csv.split(',')
+    result = []
+    for heading in headings:
+        result.append(heading.strip())
+    return result
