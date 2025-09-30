f83fdf632ddee3878d716e5a7a80a612eb6d82b2
Tanmayi D <60173146+tanmayidev@users.noreply.github.com>
2025-09-30 14:16:26 +0530

    feat: add 2025-09-30.py

---
+++ freecodecamp/daily-code-challenge/2025-09-30.py
@@ -0,0 +1,13 @@
+"""
+Phone Number Formatter
+
+Given a string of ten digits, return the string as a phone number in this format: "+D (DDD) DDD-DDDD".
+
+Tests
+Passed:1. formatNumber("05552340182") should return "+0 (555) 234-0182".
+Passed:2. formatNumber("15554354792") should return "+1 (555) 435-4792".
+"""
+
+def format_number(number):
+
+    return f"+{number[0]} ({number[1:4]}) {number[4:7]}-{number[7:]}"
