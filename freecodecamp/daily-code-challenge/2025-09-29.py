f1840f95ca77fc960bbc5129a3b31ee32e36b01d
Tanmayi D <60173146+tanmayidev@users.noreply.github.com>
2025-09-30 14:14:14 +0530

    chore: Update 2025-09-29.py

--- freecodecamp/daily-code-challenge/2025-09-29.py
+++ freecodecamp/daily-code-challenge/2025-09-29.py
@@ -7,7 +7,7 @@ Given a sentence, return the longest word in the sentence.
 - If multiple words are ties for the longest, return the first one that occurs.
 
 
-Examples : 
+Tests : 
 1. getLongestWord("coding is fun") should return "coding".
 2. getLongestWord("Coding challenges are fun and educational.") should return "educational".
 3. getLongestWord("This sentence has multiple long words.") should return "sentence".
