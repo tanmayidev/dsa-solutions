a8362c308c0e4b82acdbdb0b7290e95094c3f2a1
Tanmayi D <60173146+tanmayidev@users.noreply.github.com>
2026-01-04 23:15:39 +0530

    feat: add 2026-01-04.py

---
+++ freecodecamp/daily-code-challenge/2026-01-04.py
@@ -0,0 +1,25 @@
+"""
+Leap Year Calculator
+Given an integer year, determine whether it is a leap year.
+
+A year is a leap year if it satisfies the following rules:
+- The year is evenly divisible by 4, and
+- The year is not evenly divisible by 100, unless
+- The year is evenly divisible by 400.
+
+Test Cases : 
+1. is_leap_year(2024) should return True.
+2. is_leap_year(2023) should return False.
+3. is_leap_year(2100) should return False.
+4. is_leap_year(2000) should return True.
+5. is_leap_year(1999) should return False.
+6. is_leap_year(2040) should return True.
+7. is_leap_year(2026) should return False
+
+"""
+
+def is_leap_year(year):
+    if (year%4 == 0 and (year%100 != 0 or year%400 == 0)):
+        return True
+
+    return False
