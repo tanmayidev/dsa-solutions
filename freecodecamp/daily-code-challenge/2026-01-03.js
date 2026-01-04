1949b4a1b3cedc189f0ae92689af050cdbd71535
Tanmayi D <60173146+tanmayidev@users.noreply.github.com>
2026-01-04 10:22:35 +0530

    feat: add 2026-01-03.js

---
+++ freecodecamp/daily-code-challenge/2026-01-03.js
@@ -0,0 +1,57 @@
+/*
+Left-Handed Seat at the Table
+Given a 4x2 matrix (array of arrays) representing the seating arrangement for a meal, determine how many seats a left-handed person can sit at.
+
+A left-handed person cannot sit where a right-handed person would be in the seat to the immediate left of them.
+In the given matrix:
+
+An "R" is a seat occupied by a right-handed person.
+An "L" is a seat occupied by a left-handed person.
+An "U" is an unoccupied seat.
+Only unoccupied seats are available to sit at.
+The seats in the top row are facing "down", and the seats in the bottom row are facing "up" (like a table), so left and right are relative to the seat's orientation.
+Corner seats only have one seat next to them.
+For example, in the given matrix:
+[
+  ["U", "R", "U", "L"],
+  ["U", "R", "R", "R"]
+]
+The top-left seat is cannot be sat in because there's a right-handed person to the left. The other two open seats can be sat in because there isn't a right-handed person to the left.
+
+Test Cases : 
+1. findLeftHandedSeats([["U", "R", "U", "L"], ["U", "R", "R", "R"]]) should return 2.
+2. findLeftHandedSeats([["U", "U", "U", "U"], ["U", "U", "U", "U"]]) should return 8.
+3. findLeftHandedSeats([["U", "R", "U", "R"], ["L", "R", "R", "U"]]) should return 0.
+4. findLeftHandedSeats([["L", "U", "R", "R"], ["L", "U", "R", "R"]]) should return 1.
+5. findLeftHandedSeats([["U", "R", "U", "U"], ["U", "U", "L", "U"]]) should return 5.
+
+*/
+
+
+function findLeftHandedSeats(table) {
+
+  /*
+    For table[0] (looking down), 
+    If table[0][i+1] !== R, canOccupy++
+
+    For table[1], 
+    If table[1][i-1] !== R, canOccupy++
+  */
+  // console.log(table[1][0])
+
+  let canOccupy = 0;
+
+// Given 4x2 matrix
+  for(let i = 0; i < 4; i++) {
+      if(table[0][i] == "U" && (table[0][i+1] !== "R" ) ) {
+          canOccupy++;   
+        }
+
+      if(table[1][i] == "U" && table[1][i-1] !== "R") {
+          canOccupy++;
+        }
+  }
+
+
+  return canOccupy;
+}
