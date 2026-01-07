c676c468cc16fb34c15d94ac0708c45369035d9c
Tanmayi D <60173146+tanmayidev@users.noreply.github.com>
2026-01-07 23:25:21 +0530

    feat: add 2026-01-07.py

---
+++ freecodecamp/daily-code-challenge/2026-01-07.py
@@ -0,0 +1,29 @@
+"""
+Markdown Unordered List Parser
+Given the string of a valid unordered list in Markdown, return the equivalent HTML string.
+
+An unordered list consists of one or more list items. A valid list item appears on its own line and:
+
+Starts with a dash ("-"), followed by
+At least one space, and then
+The list item text.
+The list is given as a single string with new lines separated by the newline character ("\n"). Do not include the newline characters in the item text.
+
+Wrap each list item in HTML li tags, and the whole list of items in ul tags.
+
+For example, given "- Item A\n- Item B", return "<ul><li>Item A</li><li>Item B</li></ul>".
+
+Note: The console may not display HTML tags in strings when logging messages. Check the browser console to see logs with tags included.
+
+
+Test cases : 
+1. parseUnorderedList("- Item A\n- Item B") should return "<ul><li>Item A</li><li>Item B</li></ul>".
+2. parseUnorderedList("-  JavaScript\n-  Python") should return "<ul><li>JavaScript</li><li>Python</li></ul>".
+3. parseUnorderedList("- 2 C Flour\n- 1/2 C Sugar\n- 1 Tsp Vanilla") should return "<ul><li>2 C Flour</li><li>1/2 C Sugar</li><li>1 Tsp Vanilla</li></ul>".
+4. parseUnorderedList("- A-1\n- A-2\n- B-1") should return "<ul><li>A-1</li><li>A-2</li><li>B-1</li></ul>".
+"""
+
+def parse_unordered_list(markdown):
+    items = markdown.split('\n')
+    list_items = map(lambda item: f'<li>{item.replace("-", "", 1).strip()}</li>', items)
+    return f'<ul>{"".join(list_items)}</ul>'
