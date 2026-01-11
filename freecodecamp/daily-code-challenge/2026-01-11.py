f4f527ce1c7e3d30a21ab1a444d92d9663b5faec
Tanmayi D <60173146+tanmayidev@users.noreply.github.com>
2026-01-11 22:17:55 +0530

    feat: add 2026-01-11.py

---
+++ freecodecamp/daily-code-challenge/2026-01-11.py
@@ -0,0 +1,40 @@
+"""
+Par for the Hole
+Given two integers, the par for a golf hole and the number of strokes a golfer took on that hole, return the golfer's score using golf terms.
+
+Return:
+"Hole in one!" if it took one stroke.
+"Eagle" if it took two strokes less than par.
+"Birdie" if it took one stroke less than par.
+"Par" if it took the same number of strokes as par.
+"Bogey" if it took one stroke more than par.
+"Double bogey" if took two strokes more than par.
+
+Test Cases : 
+1. golfScore(3, 3) should return "Par".
+2. golfScore(4, 3) should return "Birdie".
+3. golfScore(3, 1) should return "Hole in one!".
+4. golfScore(5, 7) should return "Double bogey".
+5. golfScore(4, 5) should return "Bogey".
+6. golfScore(5, 3) should return "Eagle".
+"""
+
+def golf_score(par, strokes):
+
+    if strokes == 1:
+        return "Hole in one!";
+
+    diff = strokes - par;
+
+    match diff:
+      case -2: 
+        return "Eagle";
+      case -1: 
+        return "Birdie";
+      case 0: 
+        return "Par";
+      case 1: 
+        return "Bogey";
+      case 2: 
+        return "Double bogey";
+
