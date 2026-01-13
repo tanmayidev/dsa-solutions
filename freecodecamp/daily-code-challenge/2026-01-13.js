44994b59c3c611262cf02c31f15c5def58359ea3
Tanmayi D <60173146+tanmayidev@users.noreply.github.com>
2026-01-13 22:32:47 +0530

    feat: add 2026-01-13.js

---
+++ freecodecamp/daily-code-challenge/2026-01-13.js
@@ -0,0 +1,15 @@
+```
+Odd or Even?
+Given a positive integer, return "Odd" if it's an odd number, and "Even" is it's even.
+
+Test Cases : 
+1. odd_or_even(1) should return "Odd".
+2. odd_or_even(2) should return "Even".
+3. odd_or_even(13) should return "Odd".
+4. odd_or_even(196) should return "Even".
+5. odd_or_even(123456789) should return "Odd"
+```
+
+function oddOrEven(n) {
+  return (n & 1) === 0 ? "Even" : "Odd";
+}
