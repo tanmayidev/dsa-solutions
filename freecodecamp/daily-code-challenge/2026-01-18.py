42878cb7bd9ded978b9530b467a33a297c927276
Tanmayi D <60173146+tanmayidev@users.noreply.github.com>
2026-01-18 23:12:22 +0530

    feat: add 2026-01-18.py

---
+++ freecodecamp/daily-code-challenge/2026-01-18.py
@@ -0,0 +1,37 @@
+""" 
+Free Shipping
+Given an array of strings representing items in your shopping cart, and a number for the minimum order amount to qualify for free shipping, determine if the items in your shopping cart qualify for free shipping.
+
+The given array will contain items from the list below:
+
+| Item | 	Price |
+| ---- | ----- |
+| "shirt" |	34.25 |
+| "jeans" |	48.50 |
+| "shoes" |	75.00 |
+| "hat" |	19.95 |
+| "socks"	| 15.00 |
+| "jacket" |	109.95 |
+
+Test Cases : 
+1. getsFreeShipping(["shoes"], 50) should return true.
+2. getsFreeShipping(["hat", "socks"], 50) should return false.
+3. getsFreeShipping(["jeans", "shirt", "jacket"], 75) should return true.
+4. getsFreeShipping(["socks", "socks", "hat"], 75) should return false.
+5. getsFreeShipping(["shirt", "shirt", "jeans", "socks"], 100) should return true.
+6. getsFreeShipping(["hat", "socks", "hat", "jeans", "shoes", "hat"], 200) should return false.
+"""
+
+priceChart = {
+  "shirt": 34.25,
+  "jeans": 48.50,
+  "shoes": 75.00,
+  "hat": 19.95,
+  "socks": 15.00,
+  "jacket": 109.95,
+}
+
+def gets_free_shipping(cart, minimum):
+    cartPrices = map(lambda item: priceChart[item], cart)
+    cartTotal = sum(cartPrices)
+    return cartTotal > minimum
