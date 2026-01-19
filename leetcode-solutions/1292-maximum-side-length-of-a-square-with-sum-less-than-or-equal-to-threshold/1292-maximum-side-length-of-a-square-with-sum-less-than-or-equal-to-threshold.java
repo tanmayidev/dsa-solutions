f5d0a7a11c4f6592bf7bae760c16ec8e7023bbe1
Tanmayi D <60173146+tanmayidev@users.noreply.github.com>
2026-01-19 23:11:13 +0530

    Create 1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.java

---
+++ leetcode-solutions/1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.java
@@ -0,0 +1,52 @@
+class Solution {
+    public int maxSideLength(int[][] mat, int threshold) {
+        int m = mat.length;
+        int n = mat[0].length;
+
+        // Prefix sum array (1-based indexing)
+        int[][] prefix = new int[m + 1][n + 1];
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                prefix[i][j] = mat[i - 1][j - 1]
+                        + prefix[i - 1][j]
+                        + prefix[i][j - 1]
+                        - prefix[i - 1][j - 1];
+            }
+        }
+
+        int left = 0, right = Math.min(m, n);
+        int answer = 0;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (existsSquare(prefix, mid, threshold)) {
+                answer = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return answer;
+    }
+
+    private boolean existsSquare(int[][] prefix, int len, int threshold) {
+        if (len == 0) return true;
+
+        for (int i = len; i < prefix.length; i++) {
+            for (int j = len; j < prefix[0].length; j++) {
+                int sum = prefix[i][j]
+                        - prefix[i - len][j]
+                        - prefix[i][j - len]
+                        + prefix[i - len][j - len];
+
+                if (sum <= threshold) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
