fa77f333c9481489fbdd82f3418fd4addfd20a88
Tanmayi D <60173146+tanmayidev@users.noreply.github.com>
2026-01-20 23:34:43 +0530

    feat: Update 3314-construct-the-minimum-bitwise-array-i.java

--- leetcode-solutions/3314-construct-the-minimum-bitwise-array-i/3314-construct-the-minimum-bitwise-array-i.java
+++ leetcode-solutions/3314-construct-the-minimum-bitwise-array-i/3314-construct-the-minimum-bitwise-array-i.java
@@ -16,3 +16,31 @@ class Solution {
         return result;
     }
 }
+
+---------
+
+class Solution {
+    public int[] minBitwiseArray(List<Integer> nums) {
+        int n = nums.size();
+        int[] ans = new int[n];
+        
+        for (int j = 0; j < n; j++) {
+            int x = nums.get(j);
+            
+            if (x == 2) {
+                ans[j] = -1;
+            } else {
+                // Find the lowest 0-bit position (starting from bit 1)
+                for (int i = 1; i < 32; i++) {
+                    if (((x >> i) & 1) == 0) {
+                        // Clear the bit at position (i - 1)
+                        ans[j] = x ^ (1 << (i - 1));
+                        break;
+                    }
+                }
+            }
+        }
+        
+        return ans;
+    }
+}
