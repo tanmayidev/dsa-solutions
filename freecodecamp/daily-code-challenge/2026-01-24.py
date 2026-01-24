2ca9b50ad46d0cfabb09b771860e7e56ad5bd271
Tanmayi D <60173146+tanmayidev@users.noreply.github.com>
2026-01-24 23:31:10 +0530

    feat: add 2026-01-24.py

---
+++ freecodecamp/daily-code-challenge/2026-01-24.py
@@ -0,0 +1,11 @@
+def get_bingo_letter(n):
+    if (n >=1 and n <=15):
+        return "B"
+    elif (n >=16 and n<=30):
+        return "I"
+    elif (n >=31 and n<=45):
+        return "N"
+    elif (n >=46 and n<=60):
+        return "G"
+    elif (n >=61 and n<=75):
+        return "O"
