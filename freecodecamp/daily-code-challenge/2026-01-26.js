c111c9c823034186d9a8318440dad1aad3cd1c03
Tanmayi D <60173146+tanmayidev@users.noreply.github.com>
2026-01-26 22:41:58 +0530

    feat: add 2026-01-26.js

---
+++ freecodecamp/daily-code-challenge/2026-01-26.js
@@ -0,0 +1,11 @@
+function fizzBuzzMini(n) {
+  if ((n%3 == 0) && (n%5 ==0)) {
+    return "FizzBuzz";
+  }else if (n%3 == 0) {
+    return "Fizz";
+  } else if (n%5 == 0) {
+    return "Buzz";
+  } else {
+    return `${n}`
+  }
+}
