7b0019374a6f53996e84e82025a662ba729aff52
Tanmayi D <60173146+tanmayidev@users.noreply.github.com>
2026-02-13 21:50:55 +0530

    feat: add 570-managers-with-at-least-5-direct-reports.sql

---
+++ leetcode-solutions/570-managers-with-at-least-5-direct-reports/570-managers-with-at-least-5-direct-reports.sql
@@ -0,0 +1,11 @@
+-- Write your PostgreSQL query statement below
+SELECT e.name
+FROM Employee e
+JOIN (
+    SELECT managerId
+    FROM Employee
+    WHERE managerId IS NOT NULL
+    GROUP BY managerId
+    HAVING COUNT(*) >= 5
+) m
+ON e.id = m.managerId;
