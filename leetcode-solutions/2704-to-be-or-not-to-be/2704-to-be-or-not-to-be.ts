4c67fe983bac3787d7533009956f9192f2c23407
tanmayidev <tanmayi.d.99@gmail.com>
2026-03-21 18:51:54 +0530

    feat: add 2704-to-be-or-not-to-be

---
+++ leetcode-solutions/2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.ts
@@ -0,0 +1,22 @@
+type ToBeOrNotToBe = {
+    toBe: (val: any) => boolean;
+    notToBe: (val: any) => boolean;
+};
+
+function expect(val: any): ToBeOrNotToBe {
+    return {
+        toBe: (other: any) => {
+            if (val === other) return true;
+            throw new Error("Not Equal");
+        },
+        notToBe: (other: any) => {
+            if (val !== other) return true;
+            throw new Error("Equal");
+        }
+    };
+}
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
