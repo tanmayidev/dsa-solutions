817e601f0429e2fa583b7daab98bb0a544f671fc
tanmayidev <tanmayi.d.99@gmail.com>
2026-03-23 10:56:08 +0530

    feat: add hit-the-lottery

---
+++ codeforces/966A-hit-the-lottery.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int count = 0;
+
+        int[] bills = {100, 20, 10, 5, 1};
+
+        for (int bill : bills) {
+            count += n / bill;
+            n %= bill;
+        }
+
+        System.out.println(count);
+    }
+}
