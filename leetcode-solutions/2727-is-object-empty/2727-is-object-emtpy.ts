ae928c0cbf026e09f3cf341f637a6593b3d4ea0d
tanmayidev <tanmayi.d.99@gmail.com>
2026-04-04 20:21:55 +0530

    feat: update 2727

--- leetcode-solutions/2727-is-object-empty/2727-is-object-emtpy.ts
+++ leetcode-solutions/2727-is-object-empty/2727-is-object-emtpy.ts
@@ -1,3 +1,6 @@
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type Obj = Record<string, JSONValue> | JSONValue[]
+
 function isEmpty(obj: Record<string, any> | any[]): boolean {
   for (const _ in obj) {
     return false;
