1976736d13add19bdef281fe38dd8883fe23ea09
Tanmayi D <60173146+tanmayidev@users.noreply.github.com>
2026-04-06 22:32:14 +0530

    feat: leetcode 2026-04-06 ts and java

---
+++ leetcode-solutions/2619-array-prototype-last/2619-array-prototype-last.ts
@@ -0,0 +1,12 @@
+interface Array<T> {
+    last(): T | -1;
+}
+
+Array.prototype.last = function() {
+    return this.length ? this.at(-1)! : -1;
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
