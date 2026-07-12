0a1554c80ba78a4e0195065a4a27396f4ba69683
Tanmayi D <60173146+tanmayidev@users.noreply.github.com>
2026-07-12 20:27:52 +0530

    feat: LC daily 2026-07-12

---
+++ leetcode-solutions/1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+        int n = arr.length;
+        if (n == 0) return arr;
+
+        int[] sorted = arr.clone();
+        Arrays.sort(sorted);
+
+        Map<Integer, Integer> rankMap = new HashMap<>();
+        int rank = 1;
+        for (int num : sorted) {
+            if (!rankMap.containsKey(num)) {
+                rankMap.put(num, rank++);
+            }
+        }
+
+        int[] result = new int[n];
+        for (int i = 0; i < n; i++) {
+            result[i] = rankMap.get(arr[i]);
+        }
+
+        return result;
+    }
+}
